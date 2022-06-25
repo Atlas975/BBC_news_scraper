f84a62b4829afe353f9bad2a2db545aa9fac7023
Atlas975 <adilw2002@gmail.com>
2022-06-25 12:44:04 +0100

    added firefox support

--- news_scraper.py
+++ news_scraper.py
@@ -1,3 +1,19 @@
-from selenium_config import ServiceConfig
+from selenium_config import ScrapeTool
+
+
+
+
+
+
+
+
+# class BBCscraper():
+#     def
+
+
+if __name__=="__main__":
+    webBot=ScrapeTool("firefox")
+    webBot.search("https://www.bbc.co.uk/")
+
 
 
--- selenium_config.py
+++ selenium_config.py
@@ -1,14 +1,18 @@
+import webbrowser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 
-class ServiceConfig():
+class ScrapeTool():
     def __init__(self,browser="chrome"):
-        self.path="/home/adilw/Dropbox/Adil_Code/WebDrivers/"
+        self.path="/home/adilw/Dropbox/Adil_Code/WebDrivers/" # path to web driver file, webdriver name appended
         match(browser.lower()):
             case "chrome":
                 self.path+="chromedriver"
-                self.driver=webdriver.Chrome(executable_path=Service(executable_path=self.path))
+                self.driver=webdriver.Chrome(service=Service(executable_path=self.path))
+            case "edge":
+                self.path+="msedgedriver"
+                self.driver=webdriver.Edge(sevice=Service(executable_path=self.path))
             case "firefox":
                 self.path+="geckodriver"
                 self.driver=webdriver.Firefox(service=Service(executable_path=self.path))
@@ -18,6 +22,10 @@ class ServiceConfig():
             self.driver.get(website)
         except Exception:
             print("Invalid URL input")
+
+    def findelement(self):
+        self.driver.find_elements()
+        
 
 
 

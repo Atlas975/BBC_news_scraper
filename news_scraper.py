a9b709aa8748d0a428f7fbecde13686f1f6f666e
Atlas975 <adilw2002@gmail.com>
2022-06-28 16:41:57 +0100

    Auto commit

--- news_scraper.py
+++ news_scraper.py
@@ -1,23 +1,30 @@
 from selenium_config import ScrapeTool
-# import subprocess as sp
-# import argparse
+
+class BBCscraper(ScrapeTool):
+    def __init__(self,browser="chrome"):
+        ScrapeTool.__init__(self,browser, "https://www.bbc.com/news")
+        # self.element_focus('xpath','//div[@class="gs-c-promo-body gel-1/2@xs gel-1/1@m gs-u-mt@m"]')
+        # self.titleLoc='//div//a//h3'
+        # self.subtitleLoc='//div//p'
+
+    def get_article(self):
+        return self.element_text('xpath',self.titleLoc,self.subtitleLoc)
 
 
 
-# class BBCscraper():
-#     def
+
+    def article_titles(self):
+        pass
+
 
 
 if __name__=="__main__":
-    # sp.run(["pip", "install", "selenium"])
-    # strs=input()
-    # print(strs)
-    webBot=ScrapeTool("firefox","https://www.bbc.co.uk")
-    webBot.search()
-    webBot.url_append("Buisness")
-    print(webBot.url)
-    webBot.url_append("News",True)
-    print(webBot.url)
+    bbcbot=BBCscraper("firefox")
+    # bbcbot.search()
+    # BBCscraper.get_article('xpath','//div[@class="gs-c-promo-body gel-1/2@xs gel-1/1@m gs-u-mt@m"]')
+
+    # for i in bbcbot.get_article():
+    #     print(i)
 
 
     # parser = argparse.ArgumentParser(description='Pass in news category')
--- selenium_config.py
+++ selenium_config.py
@@ -1,11 +1,11 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 class ScrapeTool():
-    def __init__(self, browser = "chrome",url= "https://www.google.com"):
+    def __init__(self, browser = "chrome", url = "https://www.google.com"):
         self.path = "/home/adilw/Dropbox/Adil_Code/WebDrivers/" # path to web driver file, webdriver name appended
-        self.url=url
+        self.url = url
+        self.focus = None
         match(browser.lower()):
             case "chrome":
                 self.path += "chromedriver"
@@ -32,23 +32,20 @@ class ScrapeTool():
         except Exception:
             print("Invalid URL input")
 
-    def url_append(self, argument, replace = False):
-        if(replace):
-            self.url_remove()
-        self.url += f"/{argument}"
+    def url_append(self, argument, replace_depth = 0):
+        return f"{self.url}/{argument}" if replace_depth == 0 else f"{self.url_remove(replace_depth)}/{argument}"
 
-    def url_remove(self,depth=1):
+    def url_remove(self, depth = 1):
         for _ in range(depth):
             cutoff = self.url.rfind("/")
             self.url = self.url[:cutoff]
+        return self.url
 
-    def findelement(self):
-        self.driver.find_elements()
+    def element_focus(self, by='xpath', element='//div'):
+        self.focus = self.driver.find_elements(by, element)
 
-
-
-
-
-
+    def element_text(self,by='xpath',*args):
+        return [item.find_element(by,arg).text for item in self.focus for arg in args]
 
 
+# d
